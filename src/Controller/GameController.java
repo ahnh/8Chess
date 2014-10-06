97df1c88d4cae10d21eca2b658f25b9fed5558ca
Daniel Robinson <dan@tag.ca>
2014-10-06 13:05:26 -0400

    changed main to be in game controller

--- src/Controller/GameController.java
+++ src/Controller/GameController.java
@@ -3,4 +3,13 @@ package Controller;
 public class GameController {
 	public GameController() {
 	}
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("hello world");
+
+	}
+
 }
