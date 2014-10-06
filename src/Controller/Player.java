324a7305036ef4f3f7ec15a7cf7672e459c35999
Heesung Ahn <ahn.heesung@gmail.com>
2014-10-06 12:51:07 -0400

    update main class and put settings folder as ignore

--- src/Controller/Player.java
+++ src/Controller/Player.java
@@ -1,6 +1,13 @@
 package Controller;
 
 public class Player {
-	public Player() {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("hello world");
+
 	}
+
 }
