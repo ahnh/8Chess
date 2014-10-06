701d38321fd4f85064b2353b8e65b344f594a897
Heesung Ahn <ahn.heesung@gmail.com>
2014-10-06 18:40:59 -0400

    changed way to put a piece

--- src/Model/Piece.java
+++ src/Model/Piece.java
@@ -18,34 +18,25 @@ public class Piece {
 	}
 	
 	private void setName() {
-		if (getColor() == 0)
-		{
-			name = "W";
-		}
-		else 
-		{
-			name = "B";
-		}
-		
 		switch(getType())
 		{
 			case 0: 
-				this.name += "K";
+				this.name = "K";
 				break;
 			case 1:
-				this.name += "Q";
+				this.name = "Q";
 				break;
 			case 2:
-				this.name += "R";
+				this.name = "R";
 				break;
 			case 3:
-				this.name += "N";
+				this.name = "N";
 				break;
 			case 4:
-				this.name += "B";
+				this.name = "B";
 				break;
 			case 5:
-				this.name += "P";
+				this.name = "P";
 				break;
 		}
 		
@@ -67,11 +58,11 @@ public class Piece {
 	public String getPieceName()
 	{
 		String name = getName();
-		if (name.charAt(0) == 'B')
+		if (getColor() == 1)
 		{
 			name = name.toLowerCase();
 		}
 		
-		return String.valueOf(name.charAt(1));
+		return name;
 	}
 }
