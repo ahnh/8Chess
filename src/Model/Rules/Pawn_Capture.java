5c27ae2164c524a9f030dea1b22d70ab49102b25
Cryofox <hstances@uoguelph.ca>
2014-10-17 00:33:51 -0400

    Added Pawn_Capture, this allows Pawns to move diagonally ONLY when an enemy piece is in that square

---
+++ src/Model/Rules/Pawn_Capture.java
@@ -0,0 +1,48 @@
+package Model.Rules;
+
+import java.util.Stack;
+
+import Model.Board;
+import Model.Move;
+import Model.Rule;
+
+public class Pawn_Capture extends Rule {
+	public Pawn_Capture() {
+		super();
+	}
+	@Override
+	public int checkMove(Board board, Stack<Move> moves) {
+            
+            
+		Move currentMove = moves.peek();
+                int currentTeam = currentMove.getPiece().getTeam();
+                //This rule Only Applies to Pawns, and when they capture
+		if (currentMove.getPiece().getName().equalsIgnoreCase("pawn"))
+                {
+                 //If the Pawn is moving diagonally then we need to check, otherwise we don't care
+                 if((currentMove.getDistanceX()==1) && (currentMove.getDistanceY()==1) )    
+                 {
+                  //Can't Move Diagonally onto empty space
+                  if(board.getTile(currentMove.getEnd()).getPiece()==null)
+                    {
+                    return Rule.INVALID_MOVE;
+                    }
+                  //Can't capture piece belonging to same team
+                  else if((board.getTile(currentMove.getEnd())!=null) && (board.getTile(currentMove.getEnd()).getPiece().getTeam()==currentTeam))
+                    {
+                    return Rule.INVALID_MOVE;
+                    }
+                  
+                  
+                 }
+                 
+                }
+
+                return Rule.VALID_MOVE;
+		
+                
+                
+                
+                
+	}
+}
---
+++ src/Model/Rules/TestCheck.java
@@ -0,0 +1,74 @@
+package Model.Variants;
+
+import java.awt.Point;
+import java.util.List;
+import java.util.Stack;
+
+import Model.Board;
+import Model.Move;
+import Model.Variant;
+import Model.Pieces.*;
+import Model.Rules.*;
+
+public class TestCheck extends Variant {
+	public TestCheck(List<Board> boards) {
+		super( 2 );	
+		Board board = new Board();	
+		initBoard(board);
+		boards.add(board);
+		// Add Rules
+                rules.add(new Pawn_Move());
+                rules.add(new CollisionMove());
+		//rules.add(new Castling());
+                rules.add(new Pawn_Capture());
+                rules.add(new Check());
+                
+                
+	}
+	private void initBoard(Board board) {
+            TestCheck2(board);
+
+                    
+		
+	}
+        
+        
+        //King Not in Check, Surrounded by Pieces
+        
+        //Board doesn't need updating, just needs to check if any enemy piece can get to the king
+        //if he moves to his desired location
+        void TestCheck1(Board board)
+        {
+		// Team 1
+		int team = 1;
+		board.getTile(new Point(4,0)).setPiece(new King(team));
+                
+
+
+                
+		// Team 2
+		team = 2;
+		board.getTile(new Point(5,1)).setPiece(new Rook(team));
+		board.getTile(new Point(1,2)).setPiece(new Bishop(team));      
+        }
+        
+        //Piece is blocking check,
+        //Moving king behind piece should be allowed
+         void TestCheck2(Board board)
+        {
+		// Team 1
+		int team = 1;
+		board.getTile(new Point(4,0)).setPiece(new King(team));
+ 		board.getTile(new Point(5,1)).setPiece(new Pawn(team));               
+ 		board.getTile(new Point(0,0)).setPiece(new Queen(team));
+
+                
+		// Team 2
+		team = 2;
+		board.getTile(new Point(5,4)).setPiece(new Rook(team));   
+ 		board.getTile(new Point(5,5)).setPiece(new Queen(team));   
+                board.getTile(new Point(3,3)).setPiece(new Knight(team));
+        }       
+        
+        
+}
