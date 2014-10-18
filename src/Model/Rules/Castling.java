4dca87745964cfc8e81c72bbc209006482ce0eb2
Heesung Ahn <ahn.heesung@gmail.com>
2014-10-18 14:02:42 -0400

    fix

--- src/Model/Rules/Castling.java
+++ src/Model/Rules/Castling.java
@@ -17,11 +17,13 @@ public class Castling extends Rule {
 	@Override
 	public int checkMove(Board board, Stack<Move> moves) {
 		Move currentMove = moves.peek();
+		currentMove.getPiece();
 		Piece movingPiece = board.getTile(currentMove.getEnd()).getPiece();
-		if (!(movingPiece.getName().compareTo("King") == 0))
+		/*if (!(movingPiece.getName().compareTo("King") == 0))
 			return Rule.VALID_MOVE;
-		int currentTeam = movingPiece.getTeam();
 		
+		int currentTeam = movingPiece.getTeam();
+		*/
 		// find king
 		// find rook
 		// check if it is king?		
