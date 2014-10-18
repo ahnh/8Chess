46ca9ab1d13b2bf7074c6ce24ca81bb1adb09919
Cryofox <hstances@uoguelph.ca>
2014-10-17 23:58:08 -0400

    Created Piece Cheshire King, added to Chesh Chess, one variant done ^_^

--- src/Model/Variants/CheshireCat.java
+++ src/Model/Variants/CheshireCat.java
@@ -68,7 +68,7 @@ public class CheshireCat extends Variant {
 		board.getTile(new Point(1,0)).setPiece(new Knight(team));
 		board.getTile(new Point(2,0)).setPiece(new Bishop(team));
 		board.getTile(new Point(3,0)).setPiece(new Queen(team));
-		board.getTile(new Point(4,0)).setPiece(new King(team));
+		board.getTile(new Point(4,0)).setPiece(new CheshireKing(team));
 		board.getTile(new Point(5,0)).setPiece(new Bishop(team));
 		board.getTile(new Point(6,0)).setPiece(new Knight(team));
 		board.getTile(new Point(7,0)).setPiece(new Rook(team));
@@ -82,7 +82,7 @@ public class CheshireCat extends Variant {
 		board.getTile(new Point(1,7)).setPiece(new Knight(team));
 		board.getTile(new Point(2,7)).setPiece(new Bishop(team));
 		board.getTile(new Point(3,7)).setPiece(new Queen(team));
-		board.getTile(new Point(4,7)).setPiece(new King(team));
+		board.getTile(new Point(4,7)).setPiece(new CheshireKing(team));
 		board.getTile(new Point(5,7)).setPiece(new Bishop(team));
 		board.getTile(new Point(6,7)).setPiece(new Knight(team));
 		board.getTile(new Point(7,7)).setPiece(new Rook(team));
