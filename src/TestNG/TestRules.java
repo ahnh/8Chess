ce872bda434d4a5dc038abff960621d45813ad92
Cryofox <hstances@uoguelph.ca>
2014-10-27 02:54:31 -0400

    Added some sample collisions, and the check rule in test_Rules

--- src/TestNG/TestRules.java
+++ src/TestNG/TestRules.java
@@ -23,11 +23,12 @@ import Model.Rule;
 import Model.Pieces.*;
 
 public class TestRules {
-	Board board = new Board();
+
 	
 	@Test
 	public void Promotion() 
 	{
+		Board board = new Board();
 		Stack<Move> moves = new Stack();
 
 		// Initialize a pawn in a position to move into promotion
@@ -50,9 +51,139 @@ public class TestRules {
 		Assert.assertEquals(board.getTile( new Point(0, board.getHeight() - 1)).getPiece().getName(),"Queen","Pawn was not replaced by another piece even though promotion occurred.");
 	}
 	
+	
 	@Test
-	public void Collision(){
-		// Move non-knight over a piece
+	public void CollisionPawn(){
+		Board board = new Board();
 		
+
+		Stack<Move> moves = new Stack();
+		
+		// Initialize a pawn
+		board.getTile(new Point(0,0)).setPiece(new Pawn(1));
+		
+		//Set a Piece directly in front of pawn
+		board.getTile(new Point(0,1)).setPiece(new Pawn(1));		
+		//Set the Move
+		Move move = new Move(new Point(0, 0), new Point(0, 1), board.getTile(new Point(0,0)).getPiece());	
+		moves.push(move);
+		
+		Rule collisionMove = new Model.Rules.CollisionMove();
+		boolean actual = (collisionMove.checkMove(board, moves)==Model.Rule.VALID_MOVE);
+		//boolean actual = (()== Model.Rule.VALID_MOVE);
+		//Expecting False	
+		Assert.assertEquals(false,actual,"Pawn must not be able to move on this square");
 	}
+	
+	@Test
+	public void CollisionRook(){
+		Board board = new Board();
+		
+
+		Stack<Move> moves = new Stack();
+		
+		// Initialize a rook
+		board.getTile(new Point(0,0)).setPiece(new Rook(1));
+		
+		//Set a Piece directly in front of pawn
+		board.getTile(new Point(0,1)).setPiece(new Pawn(1));		
+		//Set the Move
+		Move move = new Move(new Point(0, 0), new Point(0, 1), board.getTile(new Point(0,0)).getPiece());	
+		moves.push(move);
+		
+		Rule collisionMove = new Model.Rules.CollisionMove();
+		boolean actual = (collisionMove.checkMove(board, moves)==Model.Rule.VALID_MOVE);
+		//boolean actual = (()== Model.Rule.VALID_MOVE);
+		//Expecting False	
+		Assert.assertEquals(false,actual,"Pawn must not be able to move on this square");
+	}	
+	@Test
+	public void CollisionBishop(){
+		Board board = new Board();
+		
+
+		Stack<Move> moves = new Stack();
+		
+		// Initialize a bishop
+		board.getTile(new Point(0,0)).setPiece(new Bishop(1));
+		
+		//Set a Piece directly in front of pawn
+		board.getTile(new Point(1,1)).setPiece(new Pawn(1));		
+		//Set the Move
+		Move move = new Move(new Point(0, 0), new Point(1, 1), board.getTile(new Point(0,0)).getPiece());	
+		moves.push(move);
+		
+		Rule collisionMove = new Model.Rules.CollisionMove();
+		boolean actual = (collisionMove.checkMove(board, moves)==Model.Rule.VALID_MOVE);
+		//boolean actual = (()== Model.Rule.VALID_MOVE);
+		//Expecting False	
+		Assert.assertEquals(false,actual,"Pawn must not be able to move on this square");
+	}
+	@Test
+	public void CollisionKingDiagonal(){
+		Board board = new Board();
+	
+		
+		Stack<Move> moves = new Stack();
+		
+		// Initialize a king
+		board.getTile(new Point(0,0)).setPiece(new King(1));
+		
+		//Set a Piece directly in front of pawn
+		board.getTile(new Point(1,1)).setPiece(new Pawn(1));		
+		//Set the Move
+		Move move = new Move(new Point(0, 0), new Point(1, 1), board.getTile(new Point(0,0)).getPiece());	
+		moves.push(move);
+		
+		Rule collisionMove = new Model.Rules.CollisionMove();
+		boolean actual = (collisionMove.checkMove(board, moves)==Model.Rule.VALID_MOVE);
+		//boolean actual = (()== Model.Rule.VALID_MOVE);
+		//Expecting False	
+		Assert.assertEquals(false,actual,"Pawn must not be able to move on this square");
+	}	
+	@Test
+	public void CollisionKingOrthogonal(){
+		Board board = new Board();
+		
+
+		Stack<Move> moves = new Stack();
+		
+		// Initialize a king
+		board.getTile(new Point(0,0)).setPiece(new King(1));
+		
+		//Set a Piece directly in front of pawn
+		board.getTile(new Point(0,1)).setPiece(new Pawn(1));		
+		//Set the Move
+		Move move = new Move(new Point(0, 0), new Point(0, 1), board.getTile(new Point(0,0)).getPiece());	
+		moves.push(move);
+		
+		Rule collisionMove = new Model.Rules.CollisionMove();
+		boolean actual = (collisionMove.checkMove(board, moves)==Model.Rule.VALID_MOVE);
+		//boolean actual = (()== Model.Rule.VALID_MOVE);
+		//Expecting False	
+		Assert.assertEquals(false,actual,"Pawn must not be able to move on this square");
+	}
+	@Test
+	public void Check(){
+		Board board = new Board();
+		
+
+		Stack<Move> moves = new Stack();
+		
+		// Initialize a Rook
+		board.getTile(new Point(0,0)).setPiece(new Rook(1));
+		
+		//Set an opposing King Diagonal of the rook
+		board.getTile(new Point(1,7)).setPiece(new King(2));		
+		//Set the Move
+		Move move = new Move(new Point(1, 7), new Point(0, 7), board.getTile(new Point(0,0)).getPiece());	
+		moves.push(move);
+		
+		Rule checkMove = new Model.Rules.Check();
+		boolean actual = (checkMove.checkMove(board, moves)==Model.Rule.INVALID_MOVE);
+		//boolean actual = (()== Model.Rule.VALID_MOVE);
+		//Expecting True, king should not be allowed to move in check, if check is returned than state is correct
+		Assert.assertEquals(true,actual,"King is not allowed to place himself in check");
+	}
+	
 }
