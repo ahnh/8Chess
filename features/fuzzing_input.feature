3298f6bb846fea75e1dca065d565eb12975b816c
Daniel Robinson <d.robinson004@gmail.com>
2014-11-05 12:04:42 -0500

    done fuzzing input

--- features/fuzzing_input.feature
+++ features/fuzzing_input.feature
@@ -8,8 +8,18 @@ Feature: Fuzzing Input
 
   Scenario: Randomized input in an expected format
     When I start classic chess
+    When I enter random input in the correct format
+    When I type "exit"
+    And the output should not contain "Exception"
+
+  Scenario: Randomized coordinates
+    When I start classic chess
     When I enter random coordinates
     When I type "exit"
     And the output should not contain "Exception"
 
-  
+  Scenario: Randomized coordinates with garbage included
+    When I start classic chess
+    When I enter random coordinates with garbage
+    When I type "exit"
+    And the output should not contain "Exception"  
--- features/support/specs.rb
+++ features/support/specs.rb
@@ -1,5 +1,3 @@
-require 'fuzzbert'
-
 Given(/^I start classic chess$/) do
   run_interactive("java -cp bin/ Controller.GameController")
   wait_for_output_to_contain "Text"
@@ -7,9 +5,6 @@ Given(/^I start classic chess$/) do
   wait_for_output_to_contain "Classic"
   type("1")
   wait_for_output_to_contain "p p p p p p p p"
-      # And I wait for output to contain "Classic."
-      # Then I type "1"
-      # And I wait for output to contain "p p p p p p p p"
 end
 
 Given(/^I make a move "(.*?)"$/) do |move|
@@ -21,8 +16,16 @@ When(/^I enter completely random input$/) do
   type(random_data)
 end
 
-When (/^I randomize coordinates$/) do
-  type(random_coordinates)
+When(/^I enter random input in the correct format$/) do
+  type(random_data_formatted)
+end
+
+When(/^I enter random coordinates$/) do
+  10.times { type(random_coordinates + "\n") }
+end
+
+When(/^I enter random coordinates with garbage$/) do
+  10.times { type(garbage + random_coordinates + garbage) }
 end
 
 def wait_for_output_to_contain(expected)
@@ -38,6 +41,20 @@ def random_data
   FuzzBert::Generators.random.yield + "\n"
 end
 
+def random_data_formatted
+  c = FuzzBert::Container.new
+  c << FuzzBert::Generators.random_fixlen(2)
+  c << FuzzBert::Generators.fixed("-")
+  c << FuzzBert::Generators.random_fixlen(2)
+  c.generator.yield  
+end
+
 def random_coordinates
-  "TODO"
+  numbers = [*'0'..'9']
+  letters = [*'A'..'Z']
+  input = letters.sample + numbers.sample + "-" + letters.sample + numbers.sample
+end
+
+def garbage
+  FuzzBert::Generators.random_fixlen([*0..9].sample).yield
 end
