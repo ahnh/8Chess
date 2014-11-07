6034ca40998456c74035bf205d6cfce498656153
Heesung Ahn <ahn.heesung@gmail.com>
2014-11-07 10:16:21 -0500

    SpockBaseAdded

---
+++ src/Spock/SpockTest.groovy
@@ -0,0 +1,15 @@
+package Spock
+import spock.lang.*
+
+class SpockTest extends spock.lang.Specification {
+	def "length of Spock's and his friends' names"() {
+		expect:
+		name.size() == length
+
+		where:
+		name     | length
+		"Spock"  | 5
+		"Kirk"   | 4
+		"Scotty" | 6
+	}
+}
